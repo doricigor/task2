3562bb6812c9e1b9d12e426a388aa02ca1673b2f
doricigor <doricigor@yahoo.com>
2020-01-22 16:41:25 +0100

    Doing perfect pixel on banner

--- js/main.js
+++ js/main.js
@@ -1,22 +1,8 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slider__mySlides");
-    let dots = document.getElementsByClassName("slider__dot");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: true,
+        arrows: false,
+        autplay: true,
+        autoplaySpeed: 2000
+    });
+});
