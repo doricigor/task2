4d7fed576d34fa28e8a07ef2e19f1c0490617bf4
doricigor <doricigor@yahoo.com>
2020-01-23 12:21:13 +0100

    Box and Testimonials done

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ $(document).ready(function () {
         autoplaySpeed: 2000
     });
 });
+
+$(document).ready(function () {
+    $('.testimonials__slider').slick({
+        dots: true,
+        arrows: false,
+        autplay: true,
+        autoplaySpeed: 2000
+    });
+});
