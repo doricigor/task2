d51907fa1b39c66fdb9b335a019814375b99cf56
doricigor <doricigor@yahoo.com>
2020-02-06 17:14:06 +0100

    Repare some details

--- js/main.js
+++ js/main.js
@@ -1,25 +1,48 @@
-// Banner slider
+
 $(document).ready(function () {
+    // Banner slider
     $('.slider').slick({
         dots: true,
         arrows: false,
         autplay: true,
         autoplaySpeed: 2000
     });
-});
-// Testimonials slider
-$(document).ready(function () {
+
+    // Testimonials slider
     $('.testimonials__slider').slick({
         dots: true,
         arrows: false,
         autplay: true,
         autoplaySpeed: 2000
     });
+
+    // Fancybox
+    $('[data-fancybox="images"]').fancybox({
+    });
+
+
+    // Tabs
+
+    $('.tabs-nav a').on('click', function (event) {
+        event.preventDefault();
+        const num = $(this).attr('href');
+        $('.tab-active').removeClass('tab-active');
+        $(this).parent().addClass('tab-active');
+        $(num).slideDown().siblings().slideUp();
+    });
 });
+
+
 // Mini navbar
 const nav = document.querySelector('.miniNav');
 const menu = document.querySelector('.miniNav__menu');
+const close = document.querySelector('.closeMenu');
 
 nav.addEventListener('click', e => {
-    menu.classList.toggle('d-block');
+    menu.classList.add('showMenu');
 });
+
+close.addEventListener('click', e => {
+    menu.classList.remove('showMenu');
+});
+
